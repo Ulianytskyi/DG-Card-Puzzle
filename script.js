a572e9480d1315f8f7cf8207fc77f333d99062ea
Artem Ulianytskyi <32933673+Ulianytskyi@users.noreply.github.com>
2023-10-12 08:56:04 +0300

    Commit 2

--- script.js
+++ script.js
@@ -1,48 +1,176 @@
-// Функція для випадкового перемішування масиву
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+const gameContainer = document.getElementById('game-container');
+const infoField = document.getElementById('info-field');
+
+let score = 0;
+infoField.innerHTML = `Score: ${score}`;
+
+let goalArray = [];
+
+function createCard() {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerText = getRandomNumber();
+    card.addEventListener('mousedown', onMouseDown);
+    card.addEventListener('touchstart', touchStart);
+    
+    return card;
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+    // return Math.floor(Math.random() * 2) + 9;
+}
+
+let currentCard = null;
+let currentValue = null;
+
+let firstCard = null;
+let firstValue = null;
+
+let secondCard = null;
+let secondValue = null;
+
+function touchStart(event) {
+    if (firstCard === null && secondCard === null) {
+        firstCard = event.target;
+        firstCard.style.backgroundColor = 'papayawhip';
+        firstValue = parseInt(firstCard.innerText);
+
+    } else if (firstCard === event.target && secondCard === null){
+        firstCard.style.backgroundColor = 'ghostwhite';
+        firstCard = null;
+        firstValue = null;
+
+
+    } else if (firstCard !== null && secondCard === null) {
+        secondCard = event.target;
+        secondValue = parseInt(secondCard.innerText);
+
+        if (firstValue > secondValue) {
+            secondCard.innerText = firstValue;
+            newValueWithTime(firstCard, 500);
+            score += firstValue;
+        } else if (firstValue < secondValue) {
+            newValueWithTime(firstCard, 500);
+            score -= firstValue;
+        }
+        
+        console.log(firstValue, secondValue);
+
+        infoField.innerHTML = `Score: ${score}`;
+        
+        firstCard.style.backgroundColor = 'ghostwhite';
+        firstCard = null;
+        firstValue = null;
+        secondCard = null;
+        secondValue = null;
+    }
+    if (firstValue === null) {
+        console.log('FirstValue is null', firstValue);
+    } else {
+        console.log('FirstValue isn`t null', firstValue);
+
     }
 }
 
-// Масив, що містить атрибути data-pattern
-const cardPatterns = ["pattern1", "pattern2", "pattern3", "pattern4", "pattern5", "pattern6", "pattern7", "pattern8", "pattern9"];
+function onMouseDown(event) {
+        currentCard = event.target;
+        currentValue = parseInt(currentCard.innerText);
+        currentCard.style.zIndex = 1;
+        
+        const onMouseMove = (e) => {
+            if (
+                e.clientX > event.clientX + 50 ||
+                e.clientX < event.clientX - 50 ||
+                e.clientY > event.clientY + 50 ||
+                e.clientY < event.clientY - 50
+            ) {
+                
+                firstCard = currentCard;
+                currentCard == null;
+            }
+            currentCard.style.transform = 'translate(' + (e.clientX - event.clientX) + 'px, ' + (e.clientY - event.clientY) + 'px)';
+        };
+        
+        const onMouseUp = () => {
+            currentCard.style.zIndex = '';
+            currentCard.style.transform = '';
+            currentCard == null;
+            document.addEventListener('mouseover', onMouseOver);
+            document.removeEventListener('mousemove', onMouseMove);
+            document.removeEventListener('mouseup', onMouseUp);
+            
+        };
+        
+        document.addEventListener('mouseover', onMouseOver);
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+    
+}
 
-// Випадкове перемішування атрибутів data-pattern
-shuffleArray(cardPatterns);
-
-// Отримуємо всі картки і встановлюємо їм перемішані атрибути data-pattern
-const cards = document.querySelectorAll('.card');
-cards.forEach((card, index) => {
-    card.dataset.pattern = cardPatterns[index];
-    // Видаляємо текст з картки
-    card.textContent = "";
-});
-
-let draggedCard = null;
-
-document.addEventListener('dragstart', (event) => {
-    if (event.target.classList.contains('card')) {
-        draggedCard = event.target;
-        event.dataTransfer.setData('text/plain', event.target.dataset.pattern);
+function onMouseOver(event) {
+    if (gameContainer.contains(event.target) && event.target !== gameContainer) {
+        secondCard = event.target;
+        let newValue = parseInt(secondCard.innerText);
+        if (firstCard !== null && firstCard !== secondCard && secondCard !== null) {
+            
+            if (currentValue > newValue) {
+                event.target.innerText = currentValue;
+                newValueWithTime(firstCard, 500);
+                score += currentValue;
+            } else if (currentValue < newValue) {
+                newValueWithTime(firstCard, 500);
+                score -= currentValue;
+            }
+            infoField.innerHTML = `Score: ${score}`;
+        }
+        secondCard = null;
     }
-});
 
-document.addEventListener('dragover', (event) => {
-    event.preventDefault();
-});
+    firstCard = null;
+    currentValue = null;
+    
+    document.removeEventListener('mouseover', onMouseOver);
+}
 
-document.addEventListener('drop', (event) => {
-    if (event.target.classList.contains('card') && draggedCard) {
-        event.preventDefault();
-        const pattern = event.dataTransfer.getData('text/plain');
-        const targetPattern = event.target.dataset.pattern;
+function newValueWithTime(firstCard, time) {
+    firstCard.innerText = '';
+    firstCard.style.opacity = 0;
+    setTimeout(function() {
+        firstCard.innerText = getRandomNumber();
+        firstCard.style.opacity = 1;
+        checkGoal();
+    }, time);
+}
 
-        // Міняємо місцями атрибути data-pattern на картках
-        event.target.dataset.pattern = pattern;
-        draggedCard.dataset.pattern = targetPattern;
 
-        draggedCard = null;
+function checkGoal() {
+    const allCards = document.querySelectorAll('.card');
+    allCards.forEach(card => {
+        if (card.innerText == 10) {
+            goalArray.push(card);
+
+        }
+    });
+    if (goalArray.length == 9) {
+        allCards.forEach(card => {
+            card.removeEventListener('mousedown', onMouseDown);
+            card.removeEventListener('touchstart', touchStart);
+            card.style.backgroundColor = 'gold';
+            card.style.fontSize = 40 + 'px';
+            card.innerText = 'WIN!';
+        });
     }
-});
+    goalArray = [];
+}
+
+function initializeGame() {
+    gameContainer.innerHTML = '';
+    for (let i = 0; i < 9; i++) {
+        const card = createCard();
+        gameContainer.appendChild(card);
+    }
+    checkGoal();
+}
+
+initializeGame();
